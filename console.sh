ca40bd94c1bfb3f37b5176b1bddd941c4645373f
taranovegor <taranovegor.dev@gmail.com>
2020-01-10 00:09:49 +0200

    rr-3: fixed container name

--- console.sh
+++ console.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker exec -it tg_bot_russian_roulette_php php /var/www/html/bin/console $1 $2 $3 $4
+docker exec -it tg_bot_nagan_php php /var/www/html/bin/console $1 $2 $3 $4
--- redeploy.sh
+++ redeploy.sh
@@ -4,9 +4,9 @@ docker-compose -f docker-compose.yml stop
 docker-compose -f docker-compose.yml build
 docker-compose -f docker-compose.yml up -d
 
-docker exec -it tg_bot_russian_roulette_php composer install
-docker exec -it tg_bot_russian_roulette_php php /var/www/html/bin/console rabbitmq-supervisor:rebuild
+docker exec -it tg_bot_nagan_php composer install
+docker exec -it tg_bot_nagan_php php /var/www/html/bin/console rabbitmq-supervisor:rebuild
 sleep 5
-docker exec -it tg_bot_russian_roulette_php php /var/www/html/bin/console rabbitmq-supervisor:control --wait-for-supervisord start
-docker exec -it tg_bot_russian_roulette_php php bin/console doctrine:migrations:migrate --allow-no-migration --no-interaction
-#docker exec -it tg_bot_russian_roulette_php php bin/console translation:update ru --force
+docker exec -it tg_bot_nagan_php php /var/www/html/bin/console rabbitmq-supervisor:control --wait-for-supervisord start
+docker exec -it tg_bot_nagan_php php bin/console doctrine:migrations:migrate --allow-no-migration --no-interaction
+#docker exec -it tg_bot_nagan_php php bin/console translation:update ru --force
