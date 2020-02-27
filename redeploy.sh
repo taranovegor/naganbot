d47c7b2f68047875695e64327d897d02f8044234
egor.taranov <egor.taranov@branderstudio.com>
2020-02-27 17:45:52 +0200

    rr-14: some non-priority fixes

--- redeploy.sh
+++ redeploy.sh
@@ -5,7 +5,7 @@ docker-compose -f docker-compose.yml -f docker-compose.dev.yml build
 docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d
 
 docker-compose exec php composer install
-docker-compose exec php php /var/www/html/bin/console rabbitmq-supervisor:rebuild
-sleep 5
-docker-compose exec php php /var/www/html/bin/console rabbitmq-supervisor:control --wait-for-supervisord start
+#docker-compose exec php php /var/www/html/bin/console rabbitmq-supervisor:rebuild
+#sleep 5
+#docker-compose exec php php /var/www/html/bin/console rabbitmq-supervisor:control --wait-for-supervisord start
 docker-compose exec php php bin/console doctrine:migrations:migrate --allow-no-migration --no-interaction
