ce904f55ac5e45ceeef0d6545b04ae085abf8f5c
egor.taranov <egor.taranov@branderstudio.com>
2020-04-26 21:09:09 +0300

    rr-18: fixed update event subscriber

--- src/EventSubscriber/UpdateSubscriber.php
+++ src/EventSubscriber/UpdateSubscriber.php
@@ -3,17 +3,19 @@
  * (c) Taranov Egor <dev@taranovegor.com>
  */
 
-namespace App\EventListener;
+namespace App\EventSubscriber;
 
 use App\Exception\CatchableException;
 use App\Exception\ChatOnlyException;
 use App\Service\TelegramUpdateHandler;
 use BoShurik\TelegramBotBundle\Event\UpdateEvent;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
- * Class CommandListener
+ * Class UpdateSubscriber
  */
-class CommandListener
+class UpdateSubscriber implements EventSubscriberInterface
 {
     /**
      * @var TelegramUpdateHandler
@@ -21,13 +23,32 @@ class CommandListener
     private TelegramUpdateHandler $dataSaver;
 
     /**
+     * @var LoggerInterface
+     */
+    private LoggerInterface $logger;
+
+    /**
      * CommandSubscriber constructor.
      *
      * @param TelegramUpdateHandler $dataSaver
+     * @param LoggerInterface       $logger
      */
-    public function __construct(TelegramUpdateHandler $dataSaver)
+    public function __construct(TelegramUpdateHandler $dataSaver, LoggerInterface $logger)
     {
         $this->dataSaver = $dataSaver;
+        $this->logger = $logger;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            UpdateEvent::class => [
+                ['onUpdate', 255],
+            ],
+        ];
     }
 
     /**
