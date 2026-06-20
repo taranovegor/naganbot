0c5374cc202569811d35867825dc90eafa28f5b3
Egor Taranov <dev@taranovegor.com>
2026-06-20 10:36:12 +0200

    the test range gets a proper target

--- config/env_test.go
+++ config/env_test.go
@@ -1,12 +1,18 @@
 package config
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestGetEnv(t *testing.T) {
-	variable := "GO111MODULE"
-	expected := "on"
-	actual := GetEnv(variable)
+	key := "NAGANBOT_TEST_ENV"
+	expected := "test_value"
+	os.Setenv(key, expected)
+	defer os.Unsetenv(key)
+
+	actual := GetEnv(key)
 	if actual != expected {
-		t.Errorf("value %s of environment variable %s does not match the expected value %s", actual, variable, expected)
+		t.Errorf("value %s of environment variable %s does not match the expected value %s", actual, key, expected)
 	}
 }
