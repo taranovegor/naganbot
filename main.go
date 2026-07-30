e1f7965458c6bba54a07ec0993b7ebb0ff496c66
Egor Taranov <dev@taranovegor.com>
2026-07-30 08:47:56 +0200

    wire main through app.New and remove the di container

--- main.go
+++ main.go
@@ -6,13 +6,8 @@ import (
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/joho/godotenv"
-	"github.com/taranovegor/naganbot/container"
+	"github.com/taranovegor/naganbot/app"
 	"github.com/taranovegor/naganbot/domain"
-	"github.com/taranovegor/naganbot/handler/callback"
-	"github.com/taranovegor/naganbot/handler/command"
-	"github.com/taranovegor/naganbot/service"
-	"github.com/taranovegor/naganbot/translator"
-	"gorm.io/gorm"
 )
 
 var Version = "development"
@@ -34,13 +29,12 @@ func main() {
 		log.Println("failed to load dotenv file")
 	}
 
-	sc, err := container.Init()
+	a, err := app.New()
 	if err != nil {
 		panic(err)
 	}
 
-	orm := sc.Get(container.ORM).(*gorm.DB)
-	err = orm.AutoMigrate(
+	err = a.ORM.AutoMigrate(
 		&domain.Chat{},
 		&domain.User{},
 		&domain.Game{},
@@ -50,96 +44,34 @@ func main() {
 		panic(err)
 	}
 
-	if orm.Migrator().HasColumn(&domain.Chat{}, "required_players") {
-		if err := orm.Exec("UPDATE chats SET required_players = 6 WHERE required_players IS NULL").Error; err != nil {
+	if a.ORM.Migrator().HasColumn(&domain.Chat{}, "required_players") {
+		if err := a.ORM.Exec("UPDATE chats SET required_players = 6 WHERE required_players IS NULL").Error; err != nil {
 			panic(err)
 		}
-		if err := orm.Migrator().AlterColumn(&domain.Chat{}, "Settings.RequiredPlayers"); err != nil {
-			panic(err)
-		}
-	}
-
-	if orm.Migrator().HasColumn(&domain.Game{}, "players_count") {
-		if err := orm.Exec("UPDATE games SET players_count = 6 WHERE players_count IS NULL").Error; err != nil {
-			panic(err)
-		}
-		if err := orm.Migrator().AlterColumn(&domain.Game{}, "PlayersCount"); err != nil {
+		if err := a.ORM.Migrator().AlterColumn(&domain.Chat{}, "Settings.RequiredPlayers"); err != nil {
 			panic(err)
 		}
 	}
 
-	trans := sc.Get(container.Translator).(*translator.Translator)
-	chatRepository := sc.Get(container.RepositoryChat).(domain.ChatRepository)
-	userRepository := sc.Get(container.RepositoryUser).(domain.UserRepository)
-	bot := sc.Get(container.Bot).(*service.Bot)
-	botApi := sc.Get(container.BotTelegram).(*tgbotapi.BotAPI)
-	cmdRegistry := sc.Get(container.CommandRegistry).(*command.Registry)
-	clbRegistry := sc.Get(container.CallbackRegistry).(*callback.Registry)
+	if a.ORM.Migrator().HasColumn(&domain.Game{}, "players_count") {
+		if err := a.ORM.Exec("UPDATE games SET players_count = 6 WHERE players_count IS NULL").Error; err != nil {
+			panic(err)
+		}
+		if err := a.ORM.Migrator().AlterColumn(&domain.Game{}, "PlayersCount"); err != nil {
+			panic(err)
+		}
+	}
 
-	log.Printf("authorized on account %s", botApi.Self.String())
+	log.Printf("authorized on account %s", a.BotAPI.Self.String())
 
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
 	u.AllowedUpdates = []string{tgbotapi.UpdateTypeMessage, tgbotapi.UpdateTypeCallbackQuery}
 
-	updates := botApi.GetUpdatesChan(u)
+	updates := a.BotAPI.GetUpdatesChan(u)
 	for update := range updates {
 		go safeExecute(func() {
-			handleUpdate(update, trans, bot, chatRepository, userRepository, cmdRegistry, clbRegistry)
+			a.HandleUpdate(update)
 		})
-	}
-}
-
-func handleUpdate(
-	update tgbotapi.Update,
-	trans *translator.Translator,
-	bot *service.Bot,
-	chatRepository domain.ChatRepository,
-	userRepository domain.UserRepository,
-	cmdRegistry *command.Registry,
-	clbRegistry *callback.Registry,
-) {
-	chat := update.FromChat()
-	if chat != nil {
-		if chat.IsPrivate() || chat.IsChannel() {
-			message := trans.Get("available only in chat", translator.Config{})
-			bot.SendMessage(chat.ID, message)
-
-			return
-		}
-
-		if err := chatRepository.Save(domain.NewChat(chat.ID, chat.Title, chat.UserName)); err != nil {
-			log.Printf("failed to save chat %d: %v", chat.ID, err)
-		}
-	}
-
-	from := update.SentFrom()
-	if from != nil {
-		if err := userRepository.Save(domain.NewUser(from.ID, from.FirstName, from.LastName, from.UserName)); err != nil {
-			log.Printf("failed to save user %d: %v", from.ID, err)
-		}
-	}
-
-	if update.Message != nil {
-		msg := update.Message
-		if !msg.IsCommand() {
-			return
-		}
-		name := msg.Command()
-		cmd, err := cmdRegistry.Find(name)
-		if err != nil {
-			log.Println(err.Error())
-			return
-		}
-		cmd.Execute(msg)
-	} else if update.CallbackQuery != nil {
-		callbackQuery := update.CallbackQuery
-		query := callback.Pattern(callbackQuery.Data)
-		hdlr, err := clbRegistry.Find(query)
-		if err != nil {
-			log.Println(err.Error())
-			return
-		}
-		hdlr.Execute(callbackQuery)
 	}
 }
